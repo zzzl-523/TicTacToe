d40e2127941c8babf26632f525a0e86f1b06c682
Jihyun Lee <zzh23@naver.com>
2023-08-31 16:26:44 +0900

    Clean up

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -85,21 +85,19 @@ function calculateWinner(squares) {
 
 function Game() {
   // state를 lift up 해서 square>board>game 가져오기
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   // map의 first:값, second:index
