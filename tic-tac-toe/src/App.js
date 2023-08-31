5aa5a6e7dfa10bbf578220d41e83c0352d2e18f8
Jihyun Lee <zzh23@naver.com>
2023-08-31 16:08:37 +0900

    Add) Next player & winner

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   function handleClick(i) {
     // 이미 클릭한 곳 재클릭 방지
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -31,8 +31,18 @@ function App() {
     setXIsNext(!xIsNext);
   }
 
+  // winner 없으면 null
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -50,6 +60,31 @@ function App() {
       </div>
     </>
   );
+}
+
+// Winner 판별 함수
+function calculateWinner(squares) {
+  // 이기는 경우의 수
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // 해당 경우의 수가 모두 O 또는 모두 X인지 확인 (a,b,c 전부 같으면 됨)
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
 }
 
 export default App;
