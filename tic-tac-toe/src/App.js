7a22e38394a90ca039230bc1bc0c7e4307a57d9d
Jihyun Lee <zzh23@naver.com>
2023-08-31 16:25:01 +0900

    Add) History

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -9,10 +9,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function App() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     // 이미 클릭한 곳 재클릭 방지
     if (squares[i] || calculateWinner(squares)) {
@@ -27,8 +24,7 @@ function App() {
       nextSquares[i] = "O";
     }
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   // winner 없으면 null
@@ -87,4 +83,51 @@ function calculateWinner(squares) {
   return null;
 }
 
-export default App;
+function Game() {
+  // state를 lift up 해서 square>board>game 가져오기
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  // map의 first:값, second:index
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to Move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+export default Game;
